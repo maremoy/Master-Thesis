c63ac1cb561f8174957e7cf0549b0079491ed8e5
maria <maremoy@live.no>
2016-02-10 17:14:47 +0100

    started using sessions in the survey

--- survey/views.py
+++ survey/views.py
@@ -7,26 +7,27 @@ from django_countries import countries
 
 from .models import Choice, Question, Submit
 
-class IndexView(generic.ListView):
-    template_name = 'survey/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('pub_date')[:]
+def index(request):
+    request.session.flush()
+    return render(request, 'survey/index.html')
 
 def results(request):
+    request.session.flush()
     return render(request, 'survey/results.html')
 
-choice_set = {}
+def cancel(request):
+    request.session.flush()
+    return render(request, 'survey/index.html')
 
-def detail(request, question_id, choice_set=choice_set):
+def detail(request, question_id):
+    if not question_id in request.session:
+        request.session[question_id]='0'
     try:
         question = Question.objects.get(pk=question_id)
     except(Question.DoesNotExist):
         return submit(request)
     percentage = 6.67*int(question_id) - 6.67
-    return render(request, 'survey/detail.html', {'question': question, 'choice_set':choice_set, 'percentage': percentage, 'countries': list(countries)})
+    return render(request, 'survey/detail.html', {'question': question, 'choice_set': request.session[question_id], 'percentage': percentage, 'countries': list(countries)})
 
 def vote(request, question_id):
     if 'previous_button' in request.POST:
@@ -45,7 +46,7 @@ def voteChoice(request, question_id, question):
         percentage = 6.67*int(question_id) - 6.67
         return render(request, 'survey/detail.html', {'question':question, 'error_message': "You didn't select a choice.", "percentage": percentage})
     else:
-        choice_set[question_id] = selected_choice
+        request.session[question_id]=selected_choice.choice_value
         return HttpResponseRedirect(reverse('survey:detail', kwargs={'question_id': int(question_id) + 1},))
 
 def voteCountryChoice(request, question_id, question):
@@ -53,23 +54,22 @@ def voteCountryChoice(request, question_id, question):
         selected_choice = request.POST['dropdown']
     except (KeyError, CountryChoice.DoesNotExist):
         percentage = 6.67*int(question_id) - 6.67
-        return render(request, 'survey/detail.html', {'question':question, 'error_message': "You didn't select a choice.", 'choice_set':choice_set, 'percentage': percentage, 'countries': list(countries)})
+        return render(request, 'survey/detail.html', {'question':question, 'error_message': "You didn't select a choice.", 'choice_set':request.session[question_id], 'percentage': percentage, 'countries': list(countries)})
     else:
-        choice_set[question_id] = selected_choice
+        request.session[question_id]=selected_choice
         return HttpResponseRedirect(reverse('survey:detail', kwargs={'question_id': int(question_id) + 1},))
 
 def submit(request):
     try:
-        submit = Submit(anglo_b = choice_set['1'], anglo_uf = choice_set['2'],
-            nordic_b = choice_set['3'], nordic_uf = choice_set['4'],
-            german_b = choice_set['5'], german_uf = choice_set['6'],
-            latin_b = choice_set['7'], latin_uf = choice_set['8'],
-            asian_b = choice_set['9'], asian_uf = choice_set['10'],
-            japanese_b = choice_set['11'], japanese_uf = choice_set['12'],
-            gender = choice_set['13'], age = choice_set['14'], country = choice_set['15'],)
+        submit = Submit(anglo_b = request.session['1'], anglo_uf = request.session['2'],
+            nordic_b = request.session['3'], nordic_uf = request.session['4'],
+            german_b = request.session['5'], german_uf = request.session['6'],
+            latin_b = request.session['7'], latin_uf = request.session['8'],
+            asian_b = request.session['9'], asian_uf = request.session['10'],
+            japanese_b = request.session['11'], japanese_uf = request.session['12'],
+            gender = request.session['13'], age = request.session['14'], country = request.session['15'],)
     except KeyError:
         return render(request, 'survey/results.html', {'error_message': "You did not answer all the questions in the survey. If you have any questionsm, pleas send an email:"})
     else:
         submit.save()
-        choice_set.clear()
         return HttpResponseRedirect(reverse('survey:results'))
